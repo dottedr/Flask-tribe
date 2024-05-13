2d85220fbdebffda4f051545c82d16759fb5e1f0
Sabina <dottedr@users.noreply.github.com>
2024-05-13 10:12:34 +0100

    Spacing

--- app/__init__.py
+++ app/__init__.py
@@ -12,18 +12,20 @@ def create_app():
     config_type = os.getenv('CONFIG_TYPE', default='config.ProcConfig')
     app.config.from_object(config_type)
 
-    # # database init
+    # database init
     # db_client = MongoClient(app.config['DB_ACCESS'])
     # database = db_client['DB_NAME']
+
     @app.after_request
     def add_header(response):
         response.headers['Content-Security-Policy'] = "sandbox; default-src 'self'; frame-ancestors 'none'"
         response.headers['Strict-Transport-Security'] = "max-age=31536000; includeSubDomains"
         response.headers['X-Frame-Options'] = 'DENY'
         response.headers['X-Content-Type-Options'] = 'nosniff'
-        
         return response
+    
     # blueprints
     app.register_blueprint(book_requests.get_blueprint())
+
 
     return app
